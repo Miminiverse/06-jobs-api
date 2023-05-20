e455c6cf3f304eaecebdafb1a796889e747dda3d
Minh Tam Pham <miminiverse@Minhs-MacBook-Pro.local>
2023-05-20 16:03:23 -0400

    week10 change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
-
+const cookieParser = require('cookie-parser')
 //extra security packages
 const helmet = require('helmet')
 const cors = require('cors')
@@ -32,6 +32,7 @@ app.use(rateLimiter({
   max: 100, // 100 requests per 15 mins
 }))
 app.use(express.json());
+app.use(cookieParser())
 app.use(helmet())
 app.use(cors())
 app.use(xss())
